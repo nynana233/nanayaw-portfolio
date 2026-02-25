1be80cad2bd2820dab93af2bb465b16b9cdd9f5a
Nana <nynana233@gmail.com>
2026-02-24 19:48:28 -0700

    fix(tabs): move tab initialization from created to mounted
    this.$children is only populated after mount, causing tabs to be empty
    when accessed in created() hook

---
+++ components/SkillSetTabs.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="tabs mb-11">
+      <ul class="flex">
+        <li v-for="(tab, i) in tabs" :key="i" class="mr-10">
+          <a href="#" @click="selectTab(tab)">
+            <div class="img-box" :class="{ 'is-active': tab.isActive }">
+              <img :src="require(`@/assets/svg/${tab.src}`)" alt="" srcset="" />
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="tab-details">
+      <slot />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      tabs: [] as Vue[],
+    }
+  },
+  mounted() {
+    this.tabs = this.$children
+  },
+  methods: {
+    selectTab(selectedTab: Vue): void {
+      this.tabs.forEach(
+        (tab) =>
+          (tab.$data.isActive = tab.$props.src === selectedTab.$props.src)
+      )
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.img-box {
+  display: inline-block;
+  color: #23cb23;
+  position: relative;
+  cursor: pointer;
+  img {
+    width: 32px;
+    height: 32px;
+    object-fit: contain;
+  }
+  &::after {
+    position: absolute;
+    bottom: -12px;
+    left: 0;
+    content: '';
+    display: inline-block;
+    width: 0;
+    height: 4px;
+    background-color: #23cb23;
+    transition: width 0.3s;
+  }
+  &:hover::after {
+    width: 40%;
+    transition: width 0.3s;
+  }
+  &.is-active::after {
+    width: 30px;
+  }
+}
+</style>
