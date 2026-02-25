2434a93cb4dbd5930f4115d5d0248b157b623c7d
Nana <nynana233@gmail.com>
2026-02-24 21:11:04 -0700

    chore(plugins): remove dead gsap plugin
    - Delete plugins/gsap.js (contained dead TweenMax/Power2 imports
      with eslint-disable comment and duplicate VueTypedJs registration)
    - Remove @/plugins/gsap from nuxt.config.js plugins array
    - VueTypedJs is already registered in plugins/vue-typed-js.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   css: ['@/assets/css/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/vue-typed-js', '@/plugins/gsap'],
+  plugins: ['@/plugins/vue-typed-js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
