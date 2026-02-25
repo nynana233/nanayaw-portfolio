1cab793255b79c8850354a8c588ea6b59f176677
Nana <nynana233@gmail.com>
2026-02-24 19:48:45 -0700

    feat(social): update social links and migrate GSAP
    - Update LinkedIn and GitHub URLs to nynana233
    - Wrap social icons in anchor tags
    - Remove Twitter icon
    - Migrate TweenMax to gsap.from()
    - Add SkillSetTab component
    - Add Logo component

---
+++ components/SkillSetTab.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-show="isActive">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    selected: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+
+  mounted() {
+    this.isActive = this.selected
+  },
+})
+</script>
